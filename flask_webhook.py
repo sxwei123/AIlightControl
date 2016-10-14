45c2b02bd5d174ebc0a051e00431e05491d7886b
leon <xw.song@outlook.com>
2016-10-14 17:45:33 +0800

    reply the entities from wit

--- flask_webhook.py
+++ flask_webhook.py
@@ -1,3 +1,30 @@
+
+from messengerbot import MessengerClient, messages, attachments, templates, elements
+import json
+
+#read messenger/witai token
+try:
+    with open("config.json") as f :
+        configs = json.load(f)
+        messenger_token = configs['fb_page_access_token']
+        wit_token = configs['witai']
+except:
+    print("open config.json error")
+
+# Manually initialize Messenger client
+messenger = MessengerClient(access_token=messenger_token)
+
+#init wit.ai
+from wit import Wit
+def send(request, response):
+    print(response['text'])
+
+actions = {
+    'send': send,
+}
+
+wit_client = Wit(access_token=wit_token, actions=actions)
+
 from flask import Flask,request
 app = Flask(__name__)
 
@@ -27,14 +54,25 @@ def messenger_get():
                     msgs = entry['messaging']
                     for msg in msgs:
                         sender_id = msg['sender']['id']
+                        recipient = messages.Recipient(recipient_id=sender_id)
                         #print msg
                         if 'message' in msg:
                             if 'text' in msg['message']:
-                                print msg['message']['text']
+                                #print msg['message']['text']
+                                #send text to witai
+                                wit_resp = wit_client.message(msg['message']['text'])
+                                print "wit response:" , wit_resp['entities']
+                                # Send text message
+                                message = messages.Message(text=json.dumps(wit_resp['entities']))
+                                messenger_req = messages.MessageRequest(recipient, message)
+                                messenger.send(messenger_req)
                 return "ok"
             except Exception as ex:
                 print ex
-    return "bad request!", 400
+                message = messages.Message(text="bad request")
+                messenger_req = messages.MessageRequest(recipient, message)
+                messenger.send(messenger_req)
+    return "bad request!"
 
 @app.errorhandler(404)
 def page_not_found(error):
